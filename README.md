86973c24e828999af2b1f64d08e6696a49cf5729
tangtangbedu <jungty6735@gmail.com>
2020-12-26 20:06:30 +0900

    middleWare 사용
    Express의 미들웨어(MiddleWare)는 HTTP의 Method 실행전 전처리 함수로 사용한다.
       --> app.get("/profile", middle, callback)으로 callback 실행할지 말지를 결정지을 수 있다.
       --> 미들웨어는 다음과 같이 구성된다. const middle = (req, res, next)=>{}
       --> middle 함수 내부에서 어떠한 조건이 충족되면 next();를 호출하는 방식으로 사용한다.
       --> const middle = (req, res, next)=>{} 로 next에 또다른 MiddleWare 함수를 넣으면, 콜백체인으로 쓸 수 있다.
       --> app.use(middle)로 모든 연결에 대해서 중간에 middleWare를 넣어줄 수 있다.

--- README.md
+++ README.md
@@ -18,4 +18,11 @@ cloning Youtube with VanillaJS and NodeJS
    res.send("Hello from home");
    }
    app.get("/", handleHome);
-6. ws
+6. npm install nodemon -D 로 -D를 붙여줌으로써 개발할 때만 쓸 수 있다.
+7. babel모듈은 JavaScript의 최신 표준 함수를 사용하여도 알아서 브라우저별 규격에 맞게 변경해주는 편리함을 제공한다.
+8. Express의 미들웨어(MiddleWare)는 HTTP의 Method 실행전 전처리 함수로 사용한다.
+   --> app.get("/profile", middle, callback)으로 callback 실행할지 말지를 결정지을 수 있다.
+   --> 미들웨어는 다음과 같이 구성된다. const middle = (req, res, next)=>{}
+   --> middle 함수 내부에서 어떠한 조건이 충족되면 next();를 호출하는 방식으로 사용한다.
+   --> const middle = (req, res, next)=>{} 로 next에 또다른 MiddleWare 함수를 넣으면, 콜백체인으로 쓸 수 있다.
+   --> app.use(middle)로 모든 연결에 대해서 중간에 middleWare를 넣어줄 수 있다.
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// const express = require('express');
 import express from "express";
 const app = express();
-
 const PORT = 4000;
 function handleListening(){
     console.log(`Listening on : http://localhost:${PORT}`);
 }
 
-
 function handleHome(req, res){
     // req : 서버입장에서 브라우저가 어떤 정보들을 보냈는지 확인가능한다. 
     // res : 어떻게 응답할 지 명세할 수 있다. 
-    // console.log(req);
     res.send("Hello from home1");
 }
+
+const betweenHome = (req, res, next)=> {
+    console.log("Between");
+    next();
+}
+
+app.use(betweenHome);
+// app.get("/", betweenHome, handleHome);
 app.get("/", handleHome);
 
-function handleProfile(req, res){
-    res.send("You are on my profile1");
-}
+function handleProfile(req, res){ res.send("You are on my profile1");}
 app.get("/profile", handleProfile);
-
-
 app.listen(PORT, handleListening);
